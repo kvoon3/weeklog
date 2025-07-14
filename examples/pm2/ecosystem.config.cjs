db45bc48ab683031f28d1ca7e9cf0cad5af5110d
kvoon <kvoon@duck.com>
2025-07-14 19:07:02 +0800

    chore: add pm2 example

---
+++ examples/pm2/ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "weeklog",
+      script: './dist/cli.js',
+      instances: 1,
+      cron_restart: "30 18 * * *",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+      args: 'input1 input2 --output outputPath',
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      merge_logs: true,
+      autorestart: true,
+    },
+  ],
+};
