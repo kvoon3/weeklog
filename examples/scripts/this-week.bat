d32fa0be4afaf9cfaa8523951470e814dd5681de
kvoon <kvoon@duck.com>
2025-07-16 14:22:52 +0800

    chore: add scripts example

---
+++ examples/scripts/this-week.bat
@@ -0,0 +1,3 @@
+@echo off
+npx weeklog c:/Users/admin/weila c:/workspace2 --output C:/Users/admin/Desktop/weila/weekly --copy
+pause
---
+++ examples/scripts/weeks-back-2.bat
@@ -0,0 +1,3 @@
+@echo off
+npx weeklog  c:/Users/admin/weila c:/workspace2 --output C:/Users/admin/Desktop/weila/weekly --weeks-back 2 --dry-run --copy
+pause
